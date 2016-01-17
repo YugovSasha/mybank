da4a6ec2663bf107ee757ea4ccd6c6d76590dee8
YugovSasha <yugov.sasha@gmail.com>
2016-01-17 19:30:55 +0300

    request

--- src/main/webapp/js/internal/controller/admin/admin-requests.controller.js
+++ src/main/webapp/js/internal/controller/admin/admin-requests.controller.js
@@ -2,52 +2,19 @@
 
     angular.module('mybank')
         .config(adminRequestsRouteConfig)
-        .controller('AdminRequestsController', AdminRequestsController)
-        .filter('propsFilter', function() {
-            return function(items, props) {
-                var out = [];
-
-                if (angular.isArray(items)) {
-                    items.forEach(function(item) {
-                        var itemMatches = false;
-
-                        var keys = Object.keys(props);
-                        for (var i = 0; i < keys.length; i++) {
-                            var prop = keys[i];
-                            var text = props[prop].toLowerCase();
-                            if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-                                itemMatches = true;
-                                break;
-                            }
-                        }
-
-                        if (itemMatches) {
-                            out.push(item);
-                        }
-                    });
-                } else {
-                    // Let the output be the input untouched
-                    out = items;
-                }
-
-                return out;
-            }
-        });
+        .controller('AdminRequestsController', AdminRequestsController);
 
     function adminRequestsRouteConfig($stateProvider) {
         $stateProvider
             .state('admin.requests', {
                 url: '/requests',
                 templateUrl: '/admin/requests',
-                controller: 'AdminRequestsController'
-                //controllerAs: 'adminRequests'
+                controller: 'AdminRequestsController',
+                controllerAs: 'adminRequests'
             })
     }
 
 
     function AdminRequestsController($scope, $http) {
-        $scope.model = {
-            people: 'test'
-        }
     }
 })();
